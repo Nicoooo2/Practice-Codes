a1ae18ecc72c9bd8ac63b4886d687b9b324b89f3
Nico Angelo <agustinnico2302@gmail.com>
2024-12-30 18:58:12 +0800

    Output "For Loop"

---
+++ Codes/Forloop.java
@@ -0,0 +1,9 @@
+package Codes;
+
+public class Forloop {
+    public static void main(String[] args) {
+        System.out.println("Hey Forloop");
+        System.out.println("Hey Forloop");
+        System.out.println("Hey Forloop");
+    }
+}
